a62cb836f59381b0c4835aa0a33f3b6387ac1d07
dario.ochoa <dario.ochoa@incluit.com>
2021-03-23 10:55:44 -0300

    funcional 1

--- download.sh
+++ download.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 clear
-i=$(wc -l list.txt | cut -d " " -f 1 )
+#i=$(wc -l list.txt | cut -d " " -f 1 )
 echo "cantidad de lineas" $i
-cat -n $1
+#cat -n $1
 
-t=5
-cat -n $1 | grep '    3' | cut -d " " -f 6 | cut -d '	' -f 2
+#cat -n $1 | grep '    3' | cut -d " " -f 6 | cut -d '	' -f 2
 #d=$(cat -n $1 | grep '    $t' | cut -d " " -f 6 | cut -d '	' -f 2)
 
 #echo $d
 
 #wget --tries=0 --show-progress $d
+for i in $(cat list.txt)
+do
+	echo 'download '$i
+	wget --tries=0 --show-progress $i
+done
